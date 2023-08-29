c48e7964e7f6f3b7b389e45a84bddcdd22128b8f
Mohd. Salahudeen <hudeensala948@gmail.com>
2023-08-30 02:24:19 +0530

    init

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  $("a.close_lrpopup").click(function () {
+    $(".lrpopup").fadeOut();
+    $(".loginblock").fadeOut();
+    $(".registerblock").fadeOut();
+  });
+  $("a.login_btn").click(function () {
+    $(".lrpopup").fadeIn();
+    $(".loginblock").fadeIn();
+    $(".registerblock").hide();
+    $(this).addClass("active");
+    $("a.register_btn").removeClass("active");
+  });
+  $("a.register_btn").click(function () {
+    $(".lrpopup").fadeIn();
+    $(".loginblock").hide();
+    $(".registerblock").fadeIn();
+    $(this).addClass("active");
+    $("a.login_btn").removeClass("active");
+  });
+});
+let x = document.getElementById("reg");
+let y = document.getElementById("phno");
+function phno() {
+  x.style.left = "-100%";
+  y.style.left = "0%";
+}
